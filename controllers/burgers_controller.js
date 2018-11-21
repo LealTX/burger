5cf947c743a81af852b8b0657d980a739f361715
LealTX <jacob.leal32@gmail.com>
2018-11-20 22:30:38 -0600

    update the app further

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
-router.get("/", function(req, res){
-    burger.all(function(data){
+router.get("/", function (req, res) {
+    burger.all(function (data) {
         const hbsObject = {
             burgers: data
         };
@@ -13,3 +13,31 @@ router.get("/", function(req, res){
         res.render("index", hbsObject);
     });
 });
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
+});
+
+module.exports = router;
